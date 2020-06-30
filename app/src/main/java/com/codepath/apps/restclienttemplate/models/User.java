9e11f410649a62bc7a1d4274eb7a877469d10305
cat-ding <catherineding28@gmail.com>
2020-06-30 16:37:16 -0400

    after tweeting from app, it will navigate back to the home timeline and display new tweet

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -4,12 +4,18 @@ import android.service.autofill.AutofillService;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
+@Parcel
 public class User {
 
     public String name;
     public String screenName;
     public String profileImageUrl;
+
+    // empty constructor needed by the Parceler library
+    public User() {
+    }
 
     public static User fromJSON(JSONObject jsonObject) throws JSONException {
         User user = new User();
